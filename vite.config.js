5b1f5a45ed0e9b14ab2d9f13f44b1cd6b0255d9d
Abhishek <abhishek.jain1@rutgers.edu>
2022-09-18 16:18:35 -0400

    Finally deployed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'http://Abhishek01051998.github.io/Meme-generator/',
   plugins: [react()]
 })
